98786da15be8e191dca3e392c00740fdd1f63a71
Lasith Manujitha <Manujitha98@gmail.com>
2023-02-03 00:12:35 +0530

    Add Routers in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./assets/css/main.css";
+import Login from "./layout/templates/main/Login";
+import { Box, Container } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Container maxWidth={false} disableGutters={true}>
+				<Box className="main_container" sx={{ background: "linear-gradient(60deg, rgba(0,133,255,1) 0%, rgba(55,159,255,1) 100%)", height: "100vh" }}>
+					<BrowserRouter>
+						<Routes>
+							<Route path='/login' element={<Login />}></Route>
+						</Routes>
+					</BrowserRouter>
+				</Box>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
