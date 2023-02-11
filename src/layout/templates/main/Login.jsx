bd03de8ac293d3ccea445953b3290d34c826b34c
Lasith Manujitha <Manujitha98@gmail.com>
2023-02-11 23:20:57 +0530

    Code refactor

--- src/layout/templates/main/Login.jsx
+++ src/layout/templates/main/Login.jsx
@@ -16,25 +16,34 @@ function Login() {
 	return (
 		<Container component="main" className="login_container" maxWidth="sm">
 			<CssBaseline />
+
 			<Typography component="h1" variant="h4" sx={{ fontWeight: "bold", color: "white" }}>
 				Welcome back !
 			</Typography>
+
 			<Typography component="h1" variant="h6" sx={{ fontWeight: "normal", color: "white" }}>
 				Please enter your credentials to Login
 			</Typography>
+
 			<Box className="login_form" component="form" onSubmit={print}>
 				<Typography component="h1" variant="h5" sx={{ fontWeight: "bold", color: "#0085ff", marginBottom: "1rem" }}>
 					Login
 				</Typography>
+
 				<TextField className="username" variant="outlined" margin="normal" required fullWidth id="username" label="Username" name="username" autoComplete="username" autoFocus />
+
 				<TextField className="password" variant="outlined" margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
+
 				<Container className="login_helper_container" sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: "1rem", marginBottom: "1rem" }}>
 					<FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
 					<Link href="#" variant="body2" sx={{ color: "#0057ff" }}>
 						Forgot password?
 					</Link>
 				</Container>
-				<Button variant="contained" type="submit" className="loginButton" sx={{width: '100%', bgcolor: '#379fff', fontSize: '18px'}}>Login</Button>
+
+				<Button variant="contained" type="submit" className="loginButton" sx={{ width: "100%", bgcolor: "#379fff", fontSize: "18px" }}>
+					Login
+				</Button>
 			</Box>
 		</Container>
 	);
