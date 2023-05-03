608125a66a1624afa3426faa3b1de62899bdb247
Lasith Manujitha <Manujitha98@gmail.com>
2023-05-03 20:54:48 +0530

    Update Login UI

--- src/layout/templates/main/Login.jsx
+++ src/layout/templates/main/Login.jsx
@@ -18,25 +18,17 @@ function Login() {
 			<CssBaseline />
 
 			<Typography component="h1" variant="h4" sx={{ fontWeight: "bold", color: "white" }}>
-				Welcome back !
-			</Typography>
-
-			<Typography component="h1" variant="h6" sx={{ fontWeight: "normal", color: "white" }}>
-				Please enter your credentials to Login
+				Sign In
 			</Typography>
 
 			<Box className="login_form" component="form" onSubmit={print}>
-				<Typography component="h1" variant="h5" sx={{ fontWeight: "bold", color: "#0085ff", marginBottom: "1rem" }}>
-					Login
-				</Typography>
-
 				<TextField className="username" variant="outlined" margin="normal" required fullWidth id="username" label="Username" name="username" autoComplete="username" autoFocus />
 
 				<TextField className="password" variant="outlined" margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
 
 				<Container className="login_helper_container" sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: "1rem", marginBottom: "1rem" }}>
 					<FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
-					<Link href="#" variant="body2" sx={{ color: "#0057ff" }}>
+					<Link href="#" variant="body2" sx={{ color: "#0057ff", textDecoration: "none"}}>
 						Forgot password?
 					</Link>
 				</Container>
