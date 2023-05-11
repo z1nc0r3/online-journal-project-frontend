bb804a2f9a44581348ec7490a4d9919d22f34e37
Lasith Manujitha <Manujitha98@gmail.com>
2023-05-11 11:42:19 +0530

    Add Admin navbar

---
+++ src/layout/components/admin/AdminHeader.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function AdminNavbar() {
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						Administrator
+					</Typography>
+					<Button color="inherit">Logout</Button>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
+}
