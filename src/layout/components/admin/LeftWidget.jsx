5d8f0fbd7cf771b7326ef8f6a78683fafd61e085
Lasith Manujitha <Manujitha98@gmail.com>
2023-05-13 11:27:53 +0530

    Update Left Widget UI

--- src/layout/components/admin/LeftWidget.jsx
+++ src/layout/components/admin/LeftWidget.jsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import { Box, Container, Button, Paper, Grid } from "@mui/material";
+import { Link } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import "../../../assets/css/main.css";
 
@@ -13,19 +13,27 @@ function LeftWidget() {
 		<Container component="main" className="left_widget_container">
 			<CssBaseline />
 
-			<Box className="left_widget" activeClassName="active">
-				<Button variant="contained" className="left_widget_button">
-					Trainee List
-				</Button>
-				<Button variant="contained" className="left_widget_button">
-					Supervisor List
-				</Button>
-				<Button variant="contained" className="left_widget_button">
-					Evaluator List
-				</Button>
-				<Button variant="contained" className="left_widget_button">
-					Create User
-				</Button>
+			<Box className="left_widget">
+				<Link to="/admin/trainee_list" className="left_widget_link">
+					<Button variant="contained" className="left_widget_button" activeClassName="active">
+						Trainee List
+					</Button>
+				</Link>
+				<Link to="/admin/supervisor_list" className="left_widget_link">
+					<Button variant="contained" className="left_widget_button">
+						Supervisor List
+					</Button>
+				</Link>
+				<Link to="/admin/evaluator_list" className="left_widget_link">
+					<Button variant="contained" className="left_widget_button">
+						Evaluator List
+					</Button>
+				</Link>
+				<Link to="/admin/create_user" className="left_widget_link">
+					<Button variant="contained" className="left_widget_button">
+						Create User
+					</Button>
+				</Link>
 			</Box>
 		</Container>
 	);
