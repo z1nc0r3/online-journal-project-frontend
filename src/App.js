af59bc54ec2c885d5839c28b1f7a898fba2f1cd2
Lasith Manujitha <Manujitha98@gmail.com>
2023-05-16 23:48:54 +0530

    Update Route

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 	return (
 		<div className="App">
 			<Container maxWidth={false} disableGutters={true}>
-				<Box className="main_container" sx={{ background: "linear-gradient(60deg, rgba(0,133,255,1) 0%, rgba(55,159,255,1) 100%)", height: "100vh", width: "100vw" }}>
+				<Box className="main_container" sx={{ background: "linear-gradient(60deg, rgba(0,133,255,1) 0%, rgba(55,159,255,1) 100%)", height: "auto", width: "auto", paddingBottom: "90px" }}>
 					<BrowserRouter>
 						<Routes>
 							<Route path='/' element={<Navigate to ="/login" />}></Route>
@@ -24,17 +24,17 @@ function App() {
 
 							<Route path='/admin/trainee_list' element={<Layout layout="admin_trainee_list"/>}></Route>
 							<Route path='/admin/trainee_list/edit/:id' element={<Layout layout="admin_trainee_edit"/>}></Route>
-							<Route path='/admin/create_user/trainee' element={<Layout layout="admin_create_user"/>}></Route>
+							<Route path='/admin/create_user/trainee' element={<Layout layout="admin_create_user" user="trainee"/>}></Route>
 							<Route path='/admin/trainee_list/delete/:id' element={<Layout layout="admin_trainee_delete"/>}></Route>
 
 							<Route path='/admin/supervisor_list' element={<Layout layout="admin_supervisor_list"/>}></Route>
 							<Route path='/admin/supervisor_list/edit/:id' element={<Layout layout="admin_supervisor_edit"/>}></Route>
-							<Route path='/admin/create_user/supervisor' element={<Layout layout="admin_create_user"/>}></Route>
+							<Route path='/admin/create_user/supervisor' element={<Layout layout="admin_create_user" user="supervisor"/>}></Route>
 							<Route path='/admin/supervisor_list/delete/:id' element={<Layout layout="admin_supervisor_delete"/>}></Route>
 
 							<Route path='/admin/evaluator_list' element={<Layout layout="admin_evaluator_list"/>}></Route>
 							<Route path='/admin/evaluator_list/edit/:id' element={<Layout layout="admin_evaluator_edit"/>}></Route>
-							<Route path='/admin/create_user/evaluator' element={<Layout layout="admin_create_user"/>}></Route>
+							<Route path='/admin/create_user/evaluator' element={<Layout layout="admin_create_user" user="evaluator"/>}></Route>
 							<Route path='/admin/evaluator_list/delete/:id' element={<Layout layout="admin_evaluator_delete"/>}></Route>
 						</Routes>
 					</BrowserRouter>
