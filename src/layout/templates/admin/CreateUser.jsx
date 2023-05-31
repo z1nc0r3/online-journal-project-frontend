d6a914893435b21642c0fd35d46450675163aeee
Lasith Manujitha <Manujitha98@gmail.com>
2023-06-01 01:25:08 +0530

    Finalize Trainee create form

--- src/layout/templates/admin/CreateUser.jsx
+++ src/layout/templates/admin/CreateUser.jsx
@@ -1,31 +1,28 @@
-import * as React from "react";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { Box, Container, Button } from "@mui/material";
-import Typography from "@mui/material/Typography";
+import React, { useState } from "react";
+import axios from "axios";
+import { Box, Container, Button, Typography, TextField, Select, MenuItem } from "@mui/material";
+import Alert from "@mui/material/Alert";
 import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import "../../../assets/css/list.css";
 import CreateMenu from "../../components/admin/CreateMenu";
 
-function CreateUser(props) {
+const CreateUser = (props) => {
 	const [formData, setFormData] = useState({
-		fullname: "",
-		regno: "",
+		fName: "",
+		regNo: "",
 		department: "",
 		address: "",
 		email: "",
 		phone: "",
-		est_name: "",
-		est_address: "",
-		training_from: "",
-		training_to: "",
-		trainingPeriod: "",
+		estName: "",
+		estAddress: "",
+		startDate: "",
+		duration: "",
 		password: "",
-		confirm_password: ""
+		confirm_password: "",
 	});
+
+	const [passwordError, setPasswordError] = useState("");
 
 	const handleChange = (e) => {
 		const { name, value } = e.target;
@@ -35,69 +32,24 @@ function CreateUser(props) {
 		}));
 	};
 
+	// verify full name
 	const handleFullNameChange = (e) => {
 		const { value } = e.target;
 		const fullNameRegex = /^[A-Za-z]+$/;
 
-		// Verify full name format (letters only)
 		if (value.match(fullNameRegex)) {
 			setFormData((prevFormData) => ({
 				...prevFormData,
-				fullname: value,
+				fName: value,
 			}));
 		}
 	};
 
-	const handleEmailChange = (e) => {
-		const { value } = e.target;
-		const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-
-		// Verify email format
-		if (value.match(emailRegex)) {
-			setFormData((prevFormData) => ({
-				...prevFormData,
-				email: value,
-			}));
-		}
-	};
-
-	const handlePasswordChange = (e) => {
-		const { value } = e.target;
-
-		// Password policy verifications
-		const hasMinimumLength = value.length >= 8;
-		const hasUppercase = /[A-Z]/.test(value);
-		const hasLowercase = /[a-z]/.test(value);
-		const hasNumber = /[0-9]/.test(value);
-		const hasSpecialCharacter = /[!@#$%^&*()]/.test(value);
-
-		// Verify password meets all requirements
-		if (hasMinimumLength && hasUppercase && hasLowercase && hasNumber && hasSpecialCharacter) {
-			setFormData((prevFormData) => ({
-				...prevFormData,
-				password: value,
-			}));
-		}
-	};
-
-	const handleConfirmPasswordChange = (e) => {
-		const { value } = e.target;
-		const { password } = formData;
-
-		// Verify if confirm password matches the password
-		if (value === password) {
-			setFormData((prevFormData) => ({
-				...prevFormData,
-				confirm_password: value,
-			}));
-		}
-	};
-
+	// verify phone number
 	const handlePhoneChange = (e) => {
 		const { value } = e.target;
-		const phoneRegex = /^[0-9]{0,10}$/; // Updated regex to allow maximum of 10 numbers
+		const phoneRegex = /^[0-9]{0,10}$/;
 
-		// Verify phone format (limit to 10 numbers)
 		if (value.match(phoneRegex)) {
 			setFormData((prevFormData) => ({
 				...prevFormData,
@@ -106,31 +58,59 @@ function CreateUser(props) {
 		}
 	};
 
-	const handleDateChange = (e) => {
-		const { name, value } = e.target;
+	// verify password
+	const handlePasswordChange = () => {
+		const value = formData.password;
 
-		setFormData((prevFormData) => ({
-			...prevFormData,
-			[name]: value,
-		}));
+		const hasMinimumLength = value.length >= 8;
+		const hasUppercase = /[A-Z]/.test(value);
+		const hasLowercase = /[a-z]/.test(value);
+		const hasNumber = /[0-9]/.test(value);
+		const hasSpecialCharacter = /[!@#$%^&*()]/.test(value);
 
-		const { training_from, training_to } = formData;
-
-		// Verify end date is ahead of start date
-		if (name === "training_to" && training_from && training_to) {
-			const isEndDateValid = new Date(training_to) > new Date(training_from);
-
-			if (!isEndDateValid) {
-				// Perform the necessary action if end date is not ahead of start date
-				// For example, show an error message or disable a submit button
-			}
+		if (!hasMinimumLength || !hasUppercase || !hasLowercase || !hasNumber || !hasSpecialCharacter) {
+			setPasswordError("Password must be in standard format.");
+			return false;
+		} else {
+			setPasswordError("");
+			return true;
 		}
 	};
 
-	const handleSubmit = (e) => {
+	// confirm password
+	const handleConfirmPasswordChange = () => {
+		if (formData.password !== formData.confirm_password) {
+			setPasswordError("Passwords do not match.");
+			return false;
+		} else {
+			setPasswordError("");
+			return true;
+		}
+	};
+
+	const handleSubmitTrainee = (e) => {
 		e.preventDefault();
-		// Perform form submission logic here
-		console.log(formData);
+
+		if (!handlePasswordChange()) {
+			setPasswordError("Password must be in standard format.");
+			return;
+		}
+
+		if (!handleConfirmPasswordChange()) {
+			setPasswordError("Passwords do not match.");
+			return;
+		}
+
+		axios
+			.post("http://127.0.0.1:8000/api/create/trainee", formData)
+			.then((response) => {
+				console.log(response.data);
+				alert("Form submitted successfully!");
+			})
+			.catch((error) => {
+				console.error(error);
+				alert("Error submitting the form. Please try again.");
+			});
 	};
 
 	switch (props.user) {
@@ -141,114 +121,81 @@ function CreateUser(props) {
 
 					<CreateMenu />
 
-					<form onSubmit={handleSubmit}>
-						<Box className="create_new_form" component="form">
+					<form onSubmit={handleSubmitTrainee}>
+						<Box className="create_new_form">
 							<Box className="create_new_form_left">
-								<Typography>Assigned Supervisor </Typography>
-								<TextField
-									variant="outlined"
-									margin="normal"
-									required
-									fullWidth
-									label="Full Name"
-									name="fullname"
-									autoFocus
-									type="text"
-									value={formData.fullname}
-									onChange={handleFullNameChange}
-								/>
+								<Typography>Full Name </Typography>
+								<TextField variant="outlined" required fullWidth name="fName" autoFocus type="text" value={formData.fName} onChange={handleFullNameChange} />
 
 								<Typography>Registration No </Typography>
-								<TextField variant="outlined" margin="normal" required fullWidth label="Registration No" name="regno" type="text" onChange={handleChange} />
+								<TextField variant="outlined" required fullWidth name="regNo" type="text" value={formData.regNo} onChange={handleChange} />
 
 								<Typography>Department </Typography>
-								<TextField variant="outlined" margin="normal" required fullWidth label="Department" name="department" type="text" value={formData.department} onChange={handleChange} />
+								<TextField variant="outlined" required fullWidth name="department" type="text" value={formData.department} onChange={handleChange} />
 
 								<Typography>Address </Typography>
-								<TextField variant="outlined" margin="normal" required fullWidth label="Address" name="address" type="text" value={formData.address} onChange={handleChange} />
+								<TextField variant="outlined" required fullWidth name="address" type="text" value={formData.address} onChange={handleChange} />
 
 								<Typography>Email </Typography>
-								<TextField variant="outlined" margin="normal" required fullWidth label="Email" name="email" type="email" value={formData.email} onChange={handleEmailChange} />
+								<TextField variant="outlined" required fullWidth name="email" type="email" value={formData.email} onChange={handleChange} />
 
 								<Typography>Phone </Typography>
-								<TextField
-									variant="outlined"
-									margin="normal"
-									required
-									fullWidth
-									label="Phone"
-									name="phone"
-									type="number"
-									value={formData.phone}
-									onChange={handlePhoneChange}
-									inputProps={{ maxLength: 10 }}
-								/>
+								<TextField variant="outlined" required fullWidth name="phone" type="number" value={formData.phone} onChange={handlePhoneChange} inputProps={{ maxLength: 10 }} />
 							</Box>
 
 							<Box className="create_new_form_right">
 								<Typography>Name of the Establishment </Typography>
-								<TextField
-									variant="outlined"
-									margin="normal"
-									required
-									fullWidth
-									label="Establishment Name"
-									name="est_name"
-									type="text"
-									value={formData.est_name}
-									onChange={handleChange}
-								/>
+								<TextField variant="outlined" required fullWidth name="estName" type="text" value={formData.estName} onChange={handleChange} />
 
 								<Typography>Address of the Establishment </Typography>
-								<TextField
-									variant="outlined"
-									margin="normal"
-									required
-									fullWidth
-									label="Establishment Address"
-									name="est_address"
-									type="text"
-									value={formData.est_address}
-									onChange={handleChange}
-								/>
+								<TextField variant="outlined" required fullWidth name="estAddress" type="text" value={formData.estAddress} onChange={handleChange} />
 
 								<Typography>Training Period </Typography>
 								<Box className="training_period" sx={{ display: "flex", flexDirection: "row" }}>
 									<Box className="training_period_from">
 										<Typography>From </Typography>
-										<TextField variant="outlined" margin="normal" required fullWidth name="training_from" type="date" value={formData.training_from} onChange={handleChange} />
+										<TextField variant="outlined" required fullWidth name="startDate" type="date" value={formData.startDate} onChange={handleChange} />
 									</Box>
 
 									<Box className="training_period_to">
-										<Typography>To </Typography>
-										<TextField variant="outlined" margin="normal" required fullWidth name="training_to" type="date" value={formData.training_to} onChange={handleDateChange} />
+										<Typography>Duration </Typography>
+										<Select variant="outlined" value={formData.duration} required fullWidth name="duration" type="text" onChange={handleChange}>
+											<MenuItem value={3}>3 Months</MenuItem>
+											<MenuItem value={6}>6 Months</MenuItem>
+											<MenuItem value={9}>9 Months</MenuItem>
+											<MenuItem value={12}>12 Months</MenuItem>
+										</Select>
 									</Box>
 								</Box>
 
 								<Typography>Password </Typography>
 								<TextField
 									variant="outlined"
-									margin="normal"
 									required
 									fullWidth
-									label="Password"
 									name="password"
 									type="password"
 									value={formData.password}
-									onChange={handlePasswordChange}
+									onChange={(e) => {
+										setFormData({ ...formData, password: e.target.value });
+										handlePasswordChange();
+									}}
 								/>
+
+								{passwordError && <Alert severity="error">{passwordError}</Alert>}
 
 								<Typography>Confirm Password </Typography>
 								<TextField
 									variant="outlined"
-									margin="normal"
 									required
 									fullWidth
-									label="Confirm Password"
 									name="confirm_password"
 									type="password"
 									value={formData.confirm_password}
-									onChange={handleConfirmPasswordChange}
+									onChange={(e) => {
+										setFormData({ ...formData, confirm_password: e.target.value });
+										handleConfirmPasswordChange();
+									}}
 								/>
 
 								<Button variant="contained" type="submit" className="register_button" sx={{ width: "100%", bgcolor: "#379fff", fontSize: "16px" }}>
@@ -259,115 +206,9 @@ function CreateUser(props) {
 					</form>
 				</Container>
 			);
-		case "supervisor":
-			return (
-				<Container component="main" className="create_new_container" maxWidth={false}>
-					<CssBaseline />
-
-					<CreateMenu />
-
-					<Box className="create_new_form" component="form">
-						<TextField className="username" variant="outlined" margin="normal" required fullWidth id="username" label="Username" name="username" autoComplete="username" autoFocus />
-
-						<TextField
-							className="password"
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							name="password"
-							label="Password"
-							type="password"
-							id="password"
-							autoComplete="current-password"
-						/>
-
-						<Container className="login_helper_container" sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: "1rem", marginBottom: "1rem" }}>
-							<FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
-							<Link href="#" variant="body2" sx={{ color: "#0057ff", textDecoration: "none" }}>
-								Forgot password?
-							</Link>
-						</Container>
-
-						<Button variant="contained" type="submit" className="loginButton" sx={{ width: "100%", bgcolor: "#379fff", fontSize: "18px" }}>
-							Login
-						</Button>
-					</Box>
-				</Container>
-			);
-		case "evaluator":
-			return (
-				<Container component="main" className="create_new_container" maxWidth={false}>
-					<CssBaseline />
-
-					<CreateMenu />
-
-					<Box className="create_new_form" component="form">
-						<TextField className="username" variant="outlined" margin="normal" required fullWidth id="username" label="Username" name="username" autoComplete="username" autoFocus />
-
-						<TextField
-							className="password"
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							name="password"
-							label="Password"
-							type="password"
-							id="password"
-							autoComplete="current-password"
-						/>
-
-						<Container className="login_helper_container" sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: "1rem", marginBottom: "1rem" }}>
-							<FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
-							<Link href="#" variant="body2" sx={{ color: "#0057ff", textDecoration: "none" }}>
-								Forgot password?
-							</Link>
-						</Container>
-
-						<Button variant="contained" type="submit" className="loginButton" sx={{ width: "100%", bgcolor: "#379fff", fontSize: "18px" }}>
-							Login
-						</Button>
-					</Box>
-				</Container>
-			);
 		default:
-			return (
-				<Container component="main" className="create_new_container" maxWidth={false}>
-					<CssBaseline />
-
-					<CreateMenu />
-
-					<Box className="create_new_form" component="form">
-						<TextField className="username" variant="outlined" margin="normal" required fullWidth id="username" label="Username" name="username" autoComplete="username" autoFocus />
-
-						<TextField
-							className="password"
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							name="password"
-							label="Password"
-							type="password"
-							id="password"
-							autoComplete="current-password"
-						/>
-
-						<Container className="login_helper_container" sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: "1rem", marginBottom: "1rem" }}>
-							<FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
-							<Link href="#" variant="body2" sx={{ color: "#0057ff", textDecoration: "none" }}>
-								Forgot password?
-							</Link>
-						</Container>
-
-						<Button variant="contained" type="submit" className="loginButton" sx={{ width: "100%", bgcolor: "#379fff", fontSize: "18px" }}>
-							Login
-						</Button>
-					</Box>
-				</Container>
-			);
+			return null;
 	}
-}
+};
 
 export default CreateUser;
