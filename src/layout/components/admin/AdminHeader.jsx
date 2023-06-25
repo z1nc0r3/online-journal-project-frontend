78ab22386641abdef49ac2fec850f222c9b344ad
Lasith Manujitha <Manujitha98@gmail.com>
2023-06-25 11:45:51 +0530

    Add App bar for Header

--- src/layout/components/admin/AdminHeader.jsx
+++ src/layout/components/admin/AdminHeader.jsx
@@ -1,21 +1,128 @@
 import * as React from "react";
+import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Drawer from "@mui/material/Drawer";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import MenuItem from "@mui/material/MenuItem";
 import LogoutButton from "../../templates/main/Logout";
+import "../../../assets/css/main.css";
 
-export default function AdminNavbar() {
-	return (
-		<Box sx={{ flexGrow: 1, marginBottom: 4 }}>
-			<AppBar position="static">
-				<Toolbar>
-					<Typography variant="h5" component="div" sx={{ flexGrow: 1, textAlign: "left", fontWeight: "light" }}>
-						Administrator
-					</Typography>
-					<LogoutButton />
-				</Toolbar>
-			</AppBar>
+const pages = ["Trainee List", "Supervisor List", "Evaluation List", "Create User"];
+
+function ResponsiveAppBar() {
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+
+	const [state, setState] = React.useState({
+		left: false,
+	});
+
+	const [activeButton, setActiveButton] = useState(0);
+
+	const toggleDrawer = (anchor, open) => (event) => {
+		if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+			return;
+		}
+
+		setState({ ...state, [anchor]: open });
+	};
+
+	const buttons = [
+		{ label: "Trainee List", path: "/admin/trainee_list" },
+		{ label: "Supervisor List", path: "/admin/supervisor_list" },
+		{ label: "Evaluator List", path: "/admin/evaluator_list" },
+		{ label: "Create User", path: "/admin/create_user/trainee" },
+	];
+
+	const list = (anchor) => (
+		<Box sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }} role="presentation" onClick={toggleDrawer(anchor, false)} onKeyDown={toggleDrawer(anchor, false)}>
+			<List>
+				{["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+					<ListItem key={text} disablePadding>
+						<ListItemButton>
+							<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItemButton>
+					</ListItem>
+				))}
+			</List>
 		</Box>
 	);
+
+	return (
+		<AppBar position="static" sx={{ marginBottom: 3 }}>
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+						<Drawer anchor={"left"} open={state["left"]} onClose={toggleDrawer("left", false)}>
+							{list("left")}
+						</Drawer>
+						<IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={toggleDrawer("left", true)} color="inherit">
+							<MenuIcon />
+						</IconButton>
+						<Typography variant="h6" component="div" sx={{ display: "flex", alignItems: "center", flexGrow: 1, textAlign: "left", fontWeight: "light", marginLeft: 1 }}>
+							Administrator
+						</Typography>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: "bottom",
+								horizontal: "left",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "left",
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: "block", md: "none" },
+							}}>
+							{pages.map((page) => (
+								<MenuItem key={page} onClick={handleCloseNavMenu}>
+									<Typography textAlign="center">{page}</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+
+					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+						<Typography variant="h6" component="div" sx={{ display: "flex", alignItems: "center", flexGrow: 1, textAlign: "left", fontWeight: "light", marginLeft: 1 }}>
+							Administrator
+						</Typography>
+					</Box>
+
+					<Box sx={{ flexGrow: 0 }}>
+						<LogoutButton />
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 }
+export default ResponsiveAppBar;
