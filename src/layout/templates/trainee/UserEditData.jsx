ccf9b9e96df0c369c08aa1d4e6801ca835f3793f
Lasith Manujitha <Manujitha98@gmail.com>
2023-07-24 10:30:00 +0530

    Update Trainee user data update logic

--- src/layout/templates/trainee/UserEditData.jsx
+++ src/layout/templates/trainee/UserEditData.jsx
@@ -1,18 +1,12 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Alert from "@mui/material/Alert";
-import Accordion from "@mui/material/Accordion";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import AccordionSummary from "@mui/material/AccordionSummary";
+import { Box, Container, Button, Typography, TextField, Select, MenuItem } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import "../../../assets/css/list.css";
-import ModeEditIcon from "@mui/icons-material/ModeEdit";
-import { Link } from "react-router-dom";
-import { Box, Container, Button, Typography, TextField, Select, MenuItem } from "@mui/material";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-
-function UserEditData() {
+const UpdateUser = () => {
 	const [formData, setFormData] = useState({
 		fName: "",
 		regNo: "",
@@ -24,19 +18,37 @@ function UserEditData() {
 		estAddress: "",
 		startDate: "",
 		duration: "",
-		password: "",
-		confirm_password: "",
 	});
 
-	const [passwordError, setPasswordError] = useState("");
+	const id = localStorage.getItem("user_id");
+
+	const getTraineeDetails = (event) => {
+		axios.get(`${process.env.REACT_APP_BACKEND_API_URL}/api/get/trainee/${id}`).then((response) => {
+			const data = response.data.user;
+
+			if (data.login_error) {
+				console.log("error");
+			} else {
+				setFormData((prevFormData) => ({
+					...prevFormData,
+					fName: data.fName,
+					regNo: data.regNo,
+					department: data.department,
+					address: data.address,
+					email: data.email,
+					phone: data.phone,
+					estName: data.estName,
+					estAddress: data.estAddress,
+					startDate: data.startDate,
+					duration: data.duration,
+				}));
+			}
+		});
+	};
 
 	useEffect(() => {
-		handlePasswordChange();
-	}, [formData.password]);
-
-	useEffect(() => {
-		handleConfirmPasswordChange();
-	}, [formData.confirm_password]);
+		getTraineeDetails();
+	}, []);
 
 	const handleChange = (e) => {
 		const { name, value } = e.target;
@@ -72,125 +84,28 @@ function UserEditData() {
 		}
 	};
 
-	// verify password
-	const handlePasswordChange = () => {
-		const value = formData.password;
-
-		const hasMinimumLength = value.length >= 8;
-		const hasUppercase = /[A-Z]/.test(value);
-		const hasLowercase = /[a-z]/.test(value);
-		const hasNumber = /[0-9]/.test(value);
-		const hasSpecialCharacter = /[!@#$%^&*()]/.test(value);
-
-		if (!hasMinimumLength || !hasUppercase || !hasLowercase || !hasNumber || !hasSpecialCharacter) {
-			setPasswordError("Password must be in standard format.");
-			return false;
-		} else {
-			setPasswordError("");
-			return true;
-		}
-	};
-
-	// confirm password
-	const handleConfirmPasswordChange = () => {
-		if (formData.password !== formData.confirm_password) {
-			setPasswordError("Passwords do not match.");
-			return false;
-		} else {
-			setPasswordError("");
-			return true;
-		}
-	};
-
-	const handleSubmit = (e, userType) => {
+	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		if (!handlePasswordChange()) {
-			setPasswordError("Password must be in standard format.");
-			return;
-		}
-
-		if (!handleConfirmPasswordChange()) {
-			setPasswordError("Passwords do not match.");
-			return;
-		}
-
-		setPasswordError("");
-
-		let url;
-		switch (userType) {
-			case "trainee":
-				url = `${process.env.REACT_APP_BACKEND_API_URL}/api/create/trainee`;
-				break;
-			case "supervisor":
-				url = `${process.env.REACT_APP_BACKEND_API_URL}/api/create/supervisor`;
-				break;
-			case "evaluator":
-				url = `${process.env.REACT_APP_BACKEND_API_URL}/api/create/evaluator`;
-				break;
-			default:
-				return;
-		}
-
 		axios
-			.post(url, formData)
+			.post(`${process.env.REACT_APP_BACKEND_API_URL}/api/update/trainee/${id}`, formData)
 			.then((response) => {
-				alert("Form submitted successfully!");
+				toast.success("User data updated Successfully. Redirecting...");
+				setTimeout(() => {
+					window.location.reload();
+				}, 3000);
 			})
 			.catch((error) => {
-				alert("Error submitting the form. Please try again.");
+				toast.error("Error updating user data. Please try again." + error);
 			});
 	};
 
-	const handleSubmitTrainee = (e) => {
-		handleSubmit(e, "trainee");
-	};
-
-	const handleSubmitSupervisor = (e) => {
-		handleSubmit(e, "supervisor");
-	};
-
-	const handleSubmitEvaluator = (e) => {
-		handleSubmit(e, "evaluator");
-	};
-
-	const passwordFields = () => {
+	const submitButton = () => {
 		return (
 			<>
-				<Typography>Password </Typography>
-				<TextField
-					variant="outlined"
-					required
-					fullWidth
-					name="password"
-					type="password"
-					onChange={(e) => {
-						setFormData({ ...formData, password: e.target.value });
-						handlePasswordChange();
-					}}
-				/>
-
-				{passwordError && (
-					<Alert severity="error" sx={{ marginTop: "10px" }}>
-						{passwordError}
-					</Alert>
-				)}
-
-				<Typography>Confirm Password </Typography>
-				<TextField
-					variant="outlined"
-					required
-					fullWidth
-					name="confirm_password"
-					type="password"
-					onChange={(e) => {
-						setFormData({ ...formData, confirm_password: e.target.value });
-						handleConfirmPasswordChange();
-					}}
-				/>
-
-				<Button variant="contained" type="submit" className="register_button" sx={{ width: "100%", bgcolor: "#379fff", fontSize: "16px" }}>
-					Create User
+				<Typography></Typography>
+				<Button variant="contained" type="submit" className="update_button" sx={{ width: "100%", bgcolor: "#379fff", fontSize: "16px" }}>
+					Update User Data
 				</Button>
 			</>
 		);
@@ -200,14 +115,14 @@ function UserEditData() {
 		return (
 			<>
 				<Typography>Department </Typography>
-				<Select variant="outlined" value={formData.department} required fullWidth name="department" type="text" onChange={handleChange}>
-					<MenuItem value={"computer_science"}>Computer Science</MenuItem>
-					<MenuItem value={"physics"}>Physics</MenuItem>
-					<MenuItem value={"zoology"}>Zoology</MenuItem>
-					<MenuItem value={"mathematics"}>Mathematics</MenuItem>
-					<MenuItem value={"statistics"}>Statistics</MenuItem>
-					<MenuItem value={"fisheries"}>Fisheries</MenuItem>
-					<MenuItem value={"biology"}>Biology</MenuItem>
+				<Select variant="outlined" value={formData.department} required fullWidth name="department" type="text" onChange={handleChange} disabled>
+					<MenuItem value={"Computer Science"}>Computer Science</MenuItem>
+					<MenuItem value={"Physics"}>Physics</MenuItem>
+					<MenuItem value={"Zoology"}>Zoology</MenuItem>
+					<MenuItem value={"Mathematics"}>Mathematics</MenuItem>
+					<MenuItem value={"Statistics"}>Statistics</MenuItem>
+					<MenuItem value={"Fisheries"}>Fisheries</MenuItem>
+					<MenuItem value={"Biology"}>Biology</MenuItem>
 				</Select>
 			</>
 		);
@@ -217,7 +132,7 @@ function UserEditData() {
 		return (
 			<>
 				<Typography>Email </Typography>
-				<TextField variant="outlined" required fullWidth name="email" type="email" value={formData.email} onChange={handleChange} />
+				<TextField component={'span'} variant="outlined" required fullWidth name="email" type="email" value={formData.email} onChange={handleChange} disabled/>
 
 				<Typography>Phone </Typography>
 				<TextField variant="outlined" required fullWidth name="phone" type="number" value={formData.phone} onChange={handlePhoneChange} inputProps={{ maxLength: 10 }} />
@@ -229,60 +144,63 @@ function UserEditData() {
 		return (
 			<>
 				<Typography>Name of the Establishment </Typography>
-				<TextField variant="outlined" required fullWidth name="estName" type="text" value={formData.estName} onChange={handleChange} />
+				<TextField variant="outlined" required fullWidth name="estName" type="text" value={formData.estName} onChange={handleChange} disabled/>
 
 				<Typography>Address of the Establishment </Typography>
-				<TextField variant="outlined" required fullWidth name="estAddress" type="text" value={formData.estAddress} onChange={handleChange} />
+				<TextField variant="outlined" required fullWidth name="estAddress" type="text" value={formData.estAddress} onChange={handleChange} disabled/>
 			</>
 		);
 	};
-    return (
+
+	return (
 		<Container component="main" className="create_new_container" maxWidth={false}>
-					<CssBaseline />
-					<form onSubmit={handleSubmitTrainee}>
-						<Box className="create_new_form">
-							<Box className="create_new_form_left">
-								<Typography>Full Name </Typography>
-								<TextField variant="outlined" required fullWidth name="fName" autoFocus type="text" value={formData.fName} onChange={handleFullNameChange} />
+			<CssBaseline />
 
-								<Typography>Registration No </Typography>
-								<TextField variant="outlined" required fullWidth name="regNo" type="text" value={formData.regNo} onChange={handleChange} />
+			<ToastContainer />
 
-								{departmentList()}
+			<form onSubmit={handleSubmit}>
+				<Box className="create_new_form">
+					<Box className="create_new_form_left">
+						<Typography>Full Name </Typography>
+						<TextField variant="outlined" required fullWidth name="fName" autoFocus type="text" value={formData.fName} onChange={handleFullNameChange} disabled/>
 
-								<Typography>Address </Typography>
-								<TextField variant="outlined" required fullWidth name="address" type="text" value={formData.address} onChange={handleChange} />
+						<Typography>Registration No </Typography>
+						<TextField variant="outlined" required fullWidth name="regNo" type="text" value={formData.regNo} onChange={handleChange} disabled/>
 
-								{emailPhoneFields()}
+						<Typography>Address </Typography>
+						<TextField variant="outlined" required fullWidth name="address" type="text" value={formData.address} onChange={handleChange} />
+
+						{emailPhoneFields()}
+					</Box>
+
+					<Box className="create_new_form_right">
+						{departmentList()}
+
+						{establishmentFields()}
+
+						<Box className="training_period" sx={{ display: "flex", flexDirection: "row" }}>
+							<Box className="training_period_from">
+								<Typography>Starting From </Typography>
+								<TextField variant="outlined" required fullWidth name="startDate" type="date" value={formData.startDate} onChange={handleChange} disabled/>
 							</Box>
 
-							<Box className="create_new_form_right">
-								{establishmentFields()}
-
-								<Box className="training_period" sx={{ display: "flex", flexDirection: "row" }}>
-									<Box className="training_period_from">
-										<Typography>Starting From </Typography>
-										<TextField variant="outlined" required fullWidth name="startDate" type="date" value={formData.startDate} onChange={handleChange} />
-									</Box>
-
-									<Box className="training_period_to">
-										<Typography>Duration </Typography>
-										<Select variant="outlined" value={formData.duration} required fullWidth name="duration" type="text" onChange={handleChange}>
-											<MenuItem value={3}>3 Months</MenuItem>
-											<MenuItem value={6}>6 Months</MenuItem>
-											<MenuItem value={9}>9 Months</MenuItem>
-											<MenuItem value={12}>12 Months</MenuItem>
-										</Select>
-									</Box>
-								</Box>
-
-								{passwordFields()}
+							<Box className="training_period_to">
+								<Typography>Duration </Typography>
+								<Select variant="outlined" value={formData.duration} required fullWidth name="duration" type="text" onChange={handleChange}disabled>
+									<MenuItem value={3}>3 Months</MenuItem>
+									<MenuItem value={6}>6 Months</MenuItem>
+									<MenuItem value={9}>9 Months</MenuItem>
+									<MenuItem value={12}>12 Months</MenuItem>
+								</Select>
 							</Box>
 						</Box>
-					</form>
-				</Container>
+
+						{submitButton()}
+					</Box>
+				</Box>
+			</form>
+		</Container>
 	);
+};
 
-}
-
-export default UserEditData;
+export default UpdateUser;
