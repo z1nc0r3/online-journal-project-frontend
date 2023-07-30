d189fd4f5552dd19bff6ce4a791e08bac587b9b1
thiloka99 <107987232+thiloka99@users.noreply.github.com>
2023-07-30 14:25:07 +0530

    supervisor dashboard UI

--- src/layout/templates/supervisor/Dashboard.jsx
+++ src/layout/templates/supervisor/Dashboard.jsx
@@ -2,12 +2,15 @@ import * as React from "react";
 import "../../../assets/css/list.css";
 import { Box, Container, Button, colors } from "@mui/material";
 import Accordion from "@mui/material/Accordion";
+import axios from "axios";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import CssBaseline from "@mui/material/CssBaseline";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import { ToastContainer, toast } from "react-toastify";
+import { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import "../../../assets/css/list.css";
 
@@ -16,97 +19,126 @@ import { Link } from "react-router-dom";
 
 function Dashboard() {
 	const [expanded, setExpanded] = React.useState(false);
+	const [trainees, setTrainees] = React.useState([]);
+	const [formData, setFormData] = useState({});
+
+	const [recordData, setRecordData] = useState({
+		user_id: "",
+		description:  localStorage.getItem("description") || "",
+		solutions: localStorage.getItem("solutions") || "",
+		week: "",
+		month: "",
+		year: "",
+	});
 
 	const handleChange = (panel) => (event, isExpanded) => {
 		setExpanded(isExpanded ? panel : false);
+		const { name, value } = event.target;
+		setRecordData((prevFormData) => ({
+			...prevFormData,
+			[name]: value,
+		}));
 	};
+
+	const getTraineeList = (event) => {
+		axios.get(`${process.env.REACT_APP_BACKEND_API_URL}/api/get/trainee/list`).then((response) => {
+			const data = response.data;
+
+			if (data.error) {
+				console.log(data.error);
+			} else {
+				const traineesData = data.trainees.reduce((acc, trainee) => {
+					acc[trainee.id] = {
+						supervisor_id: trainee.trainee_connection ? trainee.trainee_connection.supervisor_id : "",
+						supervisor: trainee.trainee_connection ? trainee.trainee_connection.supervisor_name : "",
+						evaluator_id: trainee.trainee_connection ? trainee.trainee_connection.evaluator_id : "",
+						evaluator: trainee.trainee_connection ? trainee.trainee_connection.evaluator_name : "",
+					};
+					return acc;
+				}, {});
+
+				setTrainees(data.trainees);
+				setFormData(traineesData);
+			}
+		});
+	};
+
+	useEffect(() => {
+		getTraineeList();
+		
+	}, []);
 
 	return (
 		<Container component="main" className="list_container" maxWidth={false}>
 			<CssBaseline />
 
+			<ToastContainer />
+
 			<Box className="list_box">
-				<Accordion onChange={handleChange("list_accordion")} sx={{ width: "100%", backgroundColor: "#dfefff", boxShadow: "none" }}>
+			{trainees.map((trainee, i) => (
+				<Accordion 
+					expanded={expanded === i} 
+					onChange={handleChange(i)} 
+					sx={{ width: "100%", backgroundColor: "#dfefff", boxShadow: "none", marginBottom: "10px", borderRadius: "4px" }}
+					key={i}
+					className="accordion_item">
 					
 					<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1bh-content" id="panel1bh-header">
-						<Typography sx={{ width: "75%", flexShrink: 0, fontWeight: "medium", fontSize: "18px" }}>Student Name - month</Typography>
-					</AccordionSummary>
-					<AccordionDetails>
-						<Box className="supervisor_assign_box">
-							<Typography>Week - 1</Typography>
-						</Box>
-					</AccordionDetails>
-					<AccordionDetails>
-						<Box className="supervisor_assign_box">
-							<Typography>Week - 2</Typography>
-						</Box>
-					</AccordionDetails>
-					<AccordionDetails>
-						<Box className="supervisor_assign_box">
-							<Typography>Week - 3</Typography>
-						</Box>
-					</AccordionDetails>
-					<AccordionDetails>
-						<Box className="supervisor_assign_box">
-							<Typography>Week - 4</Typography>
-						</Box>
-					</AccordionDetails>
-					<AccordionDetails>
-						<Box className="comment_box">
-							<Typography>Supervisor Comment</Typography>
-						</Box>
-					</AccordionDetails>
-
-					<Button
-              	variant="contained"
-              	type="submit"
-             	className="register_button"
-              	sx={{ width: "75%", bgcolor: "#379fff", fontSize: "18px" }}
-           		 > Save </Button>
-
-				</Accordion>
-
-				<Accordion sx={{ width: "100%", backgroundColor: "#dfefff", boxShadow: "none" }}>
-					<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1bh-content" id="panel1bh-header">	
-						<Typography sx={{ width: "75%", flexShrink: 0, fontWeight: "medium", fontSize: "18px" }}>Student Name - month</Typography>
+						<Typography sx={{ width: "5%", flexShrink: 0, fontWeight: "medium", fontSize: "16px" }}>{i + 1}</Typography>
+						<Typography sx={{ width: "66%", flexShrink: 0, fontWeight: "medium", fontSize: "16px" }}>{trainee.fName}</Typography>
+						<Typography sx={{ color: "text.secondary", fontSize: "14px" }}>{trainee.department}</Typography>
 					</AccordionSummary>
 
 					<AccordionDetails>
-						<Box className="supervisor_assign_box">
-							<Typography>Week - 1</Typography>
+						{Array.from({length: 4}, (_, i) => (
+							<Box className="supervisor_week">
+								<Accordion key={i+1}>
+									<AccordionSummary>
+										<Typography>{`Week ${i+1}`}</Typography>
+									</AccordionSummary>
+									<AccordionDetails>
+										<Typography>
+										DESCRIPTION : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+										</Typography>
+									</AccordionDetails>
+								</Accordion>
+							</Box>
+						))}
+
+						<Box>
+							<Accordion sx={{ width: "100%", backgroundColor: "#379fff", boxShadow: "none" }}>
+								<AccordionSummary  aria-controls="panel1bh-content" id="panel1bh-header">
+									<Typography sx={{ width: "75%", flexShrink: 0, fontWeight: "medium", fontSize: "18px" }}>Supervisor Report</Typography>
+								</AccordionSummary>
+								<AccordionDetails>
+									<Box >
+										<Accordion>
+											<AccordionSummary>
+												<Typography component={'span'} variant="body1" className="supervisor_box_comment_text">
+													<TextField
+														multiline rows={6} variant="outlined" required
+														fullWidth name="description" type="text" value={recordData.description}
+														placeholder="Write supervisor comments here." onChange={handleChange}
+														sx={{"& fieldset": { border: "none" },}}
+													/>
+												</Typography>
+											</AccordionSummary>
+											<AccordionDetails>
+												<Button variant="contained"  type="submit" className="register_button" sx={{ width: "95%", bgcolor: "#379fff", fontSize: "18px" }}
+												> Save </Button>
+											</AccordionDetails> 
+										</Accordion>
+									</Box>
+								</AccordionDetails>
+							</Accordion>
 						</Box>
-					</AccordionDetails>
-					<AccordionDetails>
-						<Box className="supervisor_assign_box">
-							<Typography>Week - 2</Typography>
-						</Box>
-					</AccordionDetails>
-					<AccordionDetails>
-						<Box className="supervisor_assign_box">
-							<Typography>Week - 3</Typography>
-						</Box>
-					</AccordionDetails>
-					<AccordionDetails>
-						<Box className="supervisor_assign_box">
-							<Typography>Week - 4</Typography>
-						</Box>
-					</AccordionDetails>
-					<AccordionDetails>
-						<Box className="comment_box">
-							<Typography>Supervisor Comment</Typography>
-						</Box>
+
 					</AccordionDetails>
 
-					<Button
-              	variant="contained"
-              	type="submit"
-             	className="register_button"
-              	sx={{ width: "75%", bgcolor: "#379fff", fontSize: "18px" }}
-           		 > Save </Button>
-
+					
 				</Accordion>
+				))}
 			</Box>
-
 		</Container>
 	);
 }
--- src/layout/templates/supervisor/UserEditData.jsx
+++ src/layout/templates/supervisor/UserEditData.jsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { Box, Container, Button } from "@mui/material";
 import Typography from "@mui/material/Typography";
@@ -8,24 +10,45 @@ import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import "../../../assets/css/list.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function UserEditData() {
   const [formData, setFormData] = useState({
-    fullname: "",
-    regno: "",
-    department: "",
+    fName: "",
     address: "",
-    company: "",
     email: "",
     phone: "",
     est_name: "",
     est_address: "",
-    training_from: "",
-    training_to: "",
-    trainingPeriod: "",
-    password: "",
-    confirm_password: ""
+    password: ""
   });
+
+  const { id } = useParams();
+
+  const getSupervisorDetails = (event) => {
+		axios.get(`${process.env.REACT_APP_BACKEND_API_URL}/api/get/supervisor/${id}`).then((response) => {
+			const data = response.data.user;
+
+			if (data.login_error) {
+				console.log("error");
+			} else {
+				setFormData((prevFormData) => ({
+					...prevFormData,
+					fName: data.fName,
+					address: data.address,
+					email: data.email,
+					phone: data.phone,
+					estName: data.estName,
+					estAddress: data.estAddress
+				}));
+			}
+		});
+	};
+
+  useEffect(() => {
+		getSupervisorDetails();
+	}, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -43,23 +66,86 @@ function UserEditData() {
     if (value.match(fullNameRegex) || value === "") {
       setFormData((prevFormData) => ({
         ...prevFormData,
-        fullname: value
+        fName: value
       }));
     }
   };
 
-  const handleEmailChange = (e) => {
-    const { value } = e.target;
-    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  const handlePhoneChange = (e) => {
+		const { value } = e.target;
+		const phoneRegex = /^[0-9]{0,10}$/;
 
-    // Verify email format
-    if (value.match(emailRegex) || value === "") {
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        email: value
-      }));
-    }
-  };
+		if (value.match(phoneRegex)) {
+			setFormData((prevFormData) => ({
+				...prevFormData,
+				phone: value,
+			}));
+		}
+	};
+
+  const handleSubmitSupervisor = (e) => {
+		e.preventDefault();
+
+		axios
+			.post(`${process.env.REACT_APP_BACKEND_API_URL}/api/update/supervisor/${id}`, formData)
+			.then((response) => {
+				toast.success("User data updated Successfully. Redirecting...");
+				setTimeout(() => {
+					window.location.href = "..";
+				}, 3000);
+			})
+			.catch((error) => {
+				toast.error("Error updating user data. Please try again." + error);
+			});
+	};
+
+  const updateButton = () => {
+		return (
+			<>
+				<Typography></Typography>
+				<Button variant="contained" type="submit" className="update_button" sx={{ width: "100%", bgcolor: "#379fff", fontSize: "16px" }}>
+					Update User Data
+				</Button>
+			</>
+		);
+	};
+
+  const emailPhoneFields = () => {
+		return (
+			<>
+				<Typography>Email </Typography>
+				<TextField variant="outlined" required fullWidth name="email" type="email" value={formData.email} onChange={handleChange} />
+
+				<Typography>Phone </Typography>
+				<TextField variant="outlined" required fullWidth name="phone" type="number" value={formData.phone} onChange={handlePhoneChange} inputProps={{ maxLength: 10 }} />
+			</>
+		);
+	};
+
+	const establishmentFields = () => {
+		return (
+			<>
+				<Typography>Name of the Establishment </Typography>
+				<TextField variant="outlined" required fullWidth name="estName" type="text" value={formData.estName} onChange={handleChange} />
+
+				<Typography>Address of the Establishment </Typography>
+				<TextField variant="outlined" required fullWidth name="estAddress" type="text" value={formData.estAddress} onChange={handleChange} />
+			</>
+		);
+	};
+
+  // const handleEmailChange = (e) => {
+  //   const { value } = e.target;
+  //   const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+  //   // Verify email format
+  //   if (value.match(emailRegex) || value === "") {
+  //     setFormData((prevFormData) => ({
+  //       ...prevFormData,
+  //       email: value
+  //     }));
+  //   }
+  // };
 
   const handlePasswordChange = (e) => {
     const { value } = e.target;
@@ -83,79 +169,59 @@ function UserEditData() {
     }
   };
 
-  const handleConfirmPasswordChange = (e) => {
-    const { value } = e.target;
-    const { password } = formData;
+  // const handleConfirmPasswordChange = (e) => {
+  //   const { value } = e.target;
+  //   const { password } = formData;
 
-    // Verify if confirm password matches the password
-    if (value === password || value === "") {
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        confirm_password: value
-      }));
-    }
-  };
+  //   // Verify if confirm password matches the password
+  //   if (value === password || value === "") {
+  //     setFormData((prevFormData) => ({
+  //       ...prevFormData,
+  //       confirm_password: value
+  //     }));
+  //   }
+  // };
 
-  const handlePhoneChange = (e) => {
-    const { value } = e.target;
-    const phoneRegex = /^[0-9]{0,10}$/; // Updated regex to allow a maximum of 10 numbers
+ 
 
-    // Verify phone format (limit to 10 numbers)
-    if (value.match(phoneRegex) || value === "") {
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        phone: value
-      }));
-    }
-  };
+  // const handleCompanyChange = (e) => {
+  //   const { value } = e.target;
 
-  const handleDateChange = (e) => {
-    const { name, value } = e.target;
-
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value
-    }));
-  };
-
-  const handleCompanyChange = (e) => {
-    const { value } = e.target;
-
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      company: value
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Perform form submission logic here
-    console.log(formData);
-  };
+  //   setFormData((prevFormData) => ({
+  //     ...prevFormData,
+  //     company: value
+  //   }));
+  // };
 
   return (
     <Container component="main" className="create_new_container" maxWidth={false}>
       <CssBaseline />
 
-      <form onSubmit={handleSubmit}>
+      <ToastContainer />
+
+      <form onSubmit={handleSubmitSupervisor}>
         <Box className="create_new_form" component="form">
           <Box className="create_new_form_left">
-            <h2>Change User Data</h2>
+
+            {/* <h2>Change User Data</h2> */}
+
             <Typography>Full Name</Typography>
             <TextField
               variant="outlined"
               margin="normal"
               required
-              fullWidth
-              label="Full Name"
+              fullWidth label="Full Name"
               name="fullname"
               autoFocus
               type="text"
-              value={formData.fullname}
+              value={formData.fName}
               onChange={handleFullNameChange}
             />
+            {emailPhoneFields()}
 
-            <Typography>Address</Typography>
+            </Box>
+
+            {/* <Typography>Address</Typography>
             <TextField
               variant="outlined"
               margin="normal"
@@ -166,60 +232,15 @@ function UserEditData() {
               type="text"
               value={formData.address}
               onChange={handleChange}
-            />
-
-            <Typography>Email</Typography>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              label="Email"
-              name="email"
-              type="email"
-              value={formData.email}
-              onChange={handleEmailChange}
-            />
-
-            <Typography>Phone</Typography>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              label="Phone"
-              name="phone"
-              type="number"
-              value={formData.phone}
-              onChange={handlePhoneChange}
-              inputProps={{ maxLength: 10 }}
-            />
-
-            <Typography>Company</Typography>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              label="Company"
-              name="company"
-              type="text"
-              value={formData.company}
-              onChange={handleCompanyChange}
-              inputProps={{ maxLength: 10 }}
-            />
-            
-            <Button
-              variant="contained"
-              type="submit"
-              className="register_button"
-              sx={{ width: "100%", bgcolor: "#379fff", fontSize: "16px" }}
-            >
-              Update
-            </Button>
-          </Box>
+            /> */}
 
           <Box className="create_new_form_right">
+						{establishmentFields()}
+
+						{updateButton()}
+					</Box>
+
+          {/* <Box className="create_new_form_right">
           <h2>Update Password</h2>
 
           <Typography>Current Password</Typography>
@@ -235,32 +256,6 @@ function UserEditData() {
               onChange={handlePasswordChange}
             />
 
-            <Typography>New Password</Typography>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              label="Password"
-              name="password"
-              type="password"
-              value={formData.password}
-              onChange={handlePasswordChange}
-            />
-
-            <Typography>Confirm New Password</Typography>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              label="Confirm Password"
-              name="confirm_password"
-              type="password"
-              value={formData.confirm_password}
-              onChange={handleConfirmPasswordChange}
-            />
-
             <Button
               variant="contained"
               type="submit"
@@ -269,7 +264,8 @@ function UserEditData() {
             >
               Update
             </Button>
-          </Box>
+          </Box> */}
+
         </Box>
       </form>
     </Container>
