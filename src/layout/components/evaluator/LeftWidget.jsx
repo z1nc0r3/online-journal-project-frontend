16a34330dababdd7dcb54ac117de9aa5dd230119
Jenifer-Fernando <jeniferfernando199@gmail.com>
2023-07-30 22:15:58 +0530

    update evaluator UI

--- src/layout/components/evaluator/LeftWidget.jsx
+++ src/layout/components/evaluator/LeftWidget.jsx
@@ -1,18 +1,33 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Box, Container, Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import "../../../assets/css/main.css";
 
 function LeftWidget() {
-	const [activeButton, setActiveButton] = useState(0);
+	const location = useLocation();
+	const [activeButton, setActiveButton] = useState(null);
 
 	const buttons = [
 		{ label: "DashBoard", path: "/evaluator/dashboard" },
 		{ label: "Trainee List", path: "/evaluator/trainee_list" },
 		{ label: "Edit User Data", path: "/evaluator/user_edit_data" },
 	];
+	const links = [
+		"/evaluator/dashboard",
+		"/evaluator/trainee_list",
+		"/evaluator/user_edit_data",
+		
+	]
+	useEffect(() => {
+		const index = buttons.findIndex((button) => location.pathname.includes(button.path) || (button.path.includes("/admin/create_user") && links.includes(location.pathname)));
+		setActiveButton(index);
+	}, [location.pathname]);
+
+	const handleButtonClick = (index) => {
+		setActiveButton(index);
+	};
 
 	return (
 		<Container component="main" className="left_widget_container">
@@ -21,7 +36,7 @@ function LeftWidget() {
 			<Box className="left_widget">
 				{buttons.map((button, index) => (
 					<Link key={index} to={button.path} className="left_widget_link">
-						<Button variant="contained" onClick={() => setActiveButton(index)} className={`left_widget_button ${activeButton === index ? "active" : ""}`}>
+						<Button variant="contained" onClick={() => handleButtonClick(index)} className={`left_widget_button ${activeButton === index ? "active" : ""}`}>
 							{button.label}
 						</Button>
 					</Link>
@@ -30,5 +45,6 @@ function LeftWidget() {
 		</Container>
 	);
 }
+	
 
 export default LeftWidget;
--- src/layout/templates/evaluator/Dashboard.jsx
+++ src/layout/templates/evaluator/Dashboard.jsx
@@ -2,12 +2,15 @@ import * as React from "react";
 import "../../../assets/css/list.css";
 import { Box, Container, Button, colors } from "@mui/material";
 import Accordion from "@mui/material/Accordion";
+import axios from "axios";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import CssBaseline from "@mui/material/CssBaseline";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import { ToastContainer, toast } from "react-toastify";
+import { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import "../../../assets/css/list.css";
 
@@ -15,65 +18,96 @@ import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import { Link } from "react-router-dom";
 
 function Dashboard() {
-	const [expanded, setExpanded] = React.useState(false);
-
-	const handleChange = (panel) => (event, isExpanded) => {
-		setExpanded(isExpanded ? panel : false);
+	const [expandedStudent, setExpandedStudent] = React.useState(false);
+	const [expandedMonth, setExpandedMonth] = React.useState("");
+  
+	const handleStudentChange = (panel) => (event, isExpanded) => {
+	  setExpandedStudent(isExpanded ? panel : false);
 	};
-
+  
+	const handleMonthChange = (panel) => (event, isExpanded) => {
+	  setExpandedMonth(isExpanded ? panel : "");
+	};
+  
 	return (
-		<Container component="main" className="list_container" maxWidth={false}>
-			<CssBaseline />
+	  <Container component="main" className="list_container" maxWidth={false}>
+		<CssBaseline />
+  
+		<Box className="list_box">
+		  <Accordion
+			expanded={expandedStudent}
+			onChange={handleStudentChange("panel")}
+			sx={{width: "100%",backgroundColor: "#dfefff",boxShadow: "none",}}>
+			<AccordionSummary
+			  expandIcon={<ExpandMoreIcon />}
+			  aria-controls="panel1bh-content"
+			  id="panel1bh-header"
+			>
+			  <Typography sx={{ width: "75%", flexShrink: 0, fontWeight: "medium", fontSize: "18px" }}>
+				Student Name
+			  </Typography>
+			</AccordionSummary>
 
-			<Box className="list_box">
-				<Accordion onChange={handleChange("list_accordion")} sx={{ width: "100%", backgroundColor: "#dfefff", boxShadow: "none" }}>
 					
-					<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1bh-content" id="panel1bh-header">
-						<Typography sx={{ width: "75%", flexShrink: 0, fontWeight: "medium", fontSize: "18px" }}>Student Name</Typography>
-					</AccordionSummary>
-					<AccordionDetails>
-						<Box className="evaluator_assign_box">
-						<Typography>Month - 1</Typography>
-						</Box>
-					</AccordionDetails>
-					<AccordionDetails>
-						<Box className="supervisor_assign_box">
-						<Typography>Month - 2</Typography>
-						</Box>
-					</AccordionDetails>
-					<AccordionDetails>
-						<Box className="supervisor_assign_box">
-							<Typography>Month - 3</Typography>
-						</Box>
-					</AccordionDetails>
-					<AccordionDetails>
-						<Box className="supervisor_assign_box">
-							<Typography>Month- 4</Typography>
-						</Box>
-					</AccordionDetails>
-					<AccordionDetails>
-						<Box className="supervisor_assign_box">
-							<Typography>Month- 5</Typography>
-						</Box>
-					</AccordionDetails>
-					 {/*<AccordionDetails>
-						<Box className="comment_box">
-							<Typography>Evaluator Comment</Typography>
-						</Box>
-					</AccordionDetails>
 
-					<Button
-              	variant="contained"
-              	type="submit"
-             	className="register_button"
-              	sx={{ width: "75%", bgcolor: "#379fff", fontSize: "18px" }}
-	             	 Save </Button>*/}
+						
+			<AccordionDetails>
 
+{Array.from({length: 5}, (_, i) => (
+<Box className="list_container">  
+<Accordion className="month_item">
+	<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="month1-content" id="month1-header">
+		<Typography sx={{ fontWeight: "bold", textAlign: "center", color: "#414141", paddingLeft: 0.5 }}>{`Month ${i+1}`}</Typography>
+	</AccordionSummary>
+
+	<AccordionDetails>
+		{Array.from({length: 4}, (_, i) => (
+		<Box className="list_container">
+			<Accordion sx={{ width: "100%", backgroundColor: "#9dd0ff", boxShadow: "none", marginBottom: "10px", borderRadius: "4px" }} className="accordion_item">
+				<AccordionSummary>
+					<Typography sx={{ width: "100%", flexShrink: 0, fontWeight: "medium", fontSize: "16px" }}>{`Week ${i+1}`}</Typography>
+				</AccordionSummary>
+				<AccordionDetails  className="report_detail_container">
+					<Box className="weekly_report_container">
+						<Typography className="report_title report_title_des">Description :</Typography>
+					<Box className="weekly_report_des">
+						<Typography sx={{ fontSize: "16px", textAlign: "left" }}></Typography>
+					</Box>
+					</Box>
+				</AccordionDetails>
+			</Accordion>
+		</Box>
+	))}
+
+<Box className="list_container_xx">
+<Accordion sx={{width: "100%",backgroundColor: "#65aaee",boxShadow: "none",}}>
+<AccordionSummary>
+<Typography>Supervisor Report</Typography>
+</AccordionSummary>
+</Accordion>
+</Box>
+
+</AccordionDetails>
+</Accordion>
+</Box>
+))}
+</AccordionDetails>
+<Box className="list_container_xx">
+<Accordion sx={{width: "100%",backgroundColor: "#65aaee",boxShadow: "none",}}>
+<AccordionSummary>
+<Typography>Evaluator Report</Typography>
+</AccordionSummary>
+<AccordionDetails>
+<textarea id="w3review" name="w3review" rows="8" cols="75"></textarea>
+<Button variant="contained" type="submit" className="register_button" sx={{ width: "99%", bgcolor: "#379fff", fontSize: "16px" }}>Update</Button>
+</AccordionDetails>
+</Accordion>
+</Box>
 				</Accordion>
-
+				
+			
 				
 			</Box>
-
 		</Container>
 	);
 }
