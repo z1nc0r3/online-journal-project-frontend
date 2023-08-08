9f8351ef8cf6de1c81857bef28198686588b3eec
Sandas Gimhana <109182524+sandazz@users.noreply.github.com>
2023-08-08 15:35:53 +0530

    update Record with in week - update existing record if record already in database within relevant week - get the existing record and show in the dashboard - change the button update and submit according to the record availability

--- src/layout/templates/trainee/Dashboard.jsx
+++ src/layout/templates/trainee/Dashboard.jsx
@@ -25,6 +25,8 @@ function Dashboard() {
 		year: "",
 	});
 
+	const [isLoading, setIsLoading] = useState(true);
+	const [recordExists, setRecordExists] = useState(false);
 	const [isChecked, setIsChecked] = useState(false);
 
 	useEffect(() => {
@@ -40,34 +42,48 @@ function Dashboard() {
 		Cookies.set("solutions", recordData.solutions);
 	}, [recordData]);
 
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setRecordData((prevFormData) => ({
-			...prevFormData,
-			[name]: value,
-		}));
-	};
 
-	const getConnection = (event) => {
-			axios.get(`${API_URL}/api/get/connection/trainee/${recordData.user_id}`).then((response) => {
-				const data = response.data.records;
-				console.log(data);
+	useEffect(() => {
+		const getConnection = async () => {
+		  const response = await axios.get(
+			`${API_URL}/api/get/connection/trainee/${recordData.user_id}`
+		  );
+		  const data = response.data.records;
 	
-				if (data.error) {
-					console.log(data.error);
-				} else {
-					recordData.supervisor_id = data.supervisor_id;
-					recordData.evaluator_id = data.evaluator_id;
-				}
-			});
-
-		console.log(recordData);
-
-	};
-
-	useEffect(()=>{
+		  if (!data.error) {
+			setRecordData((prevData) => ({
+			  ...prevData,
+			  supervisor_id: data.supervisor_id,
+			  evaluator_id: data.evaluator_id,
+			}));
+		  }
+		};
+	
+		const getCurrentWeekRecord = async () => {
+		  const weekDetails = getWeekInfo(new Date());
+		  const response = await axios.get(
+			`${process.env.REACT_APP_BACKEND_API_URL}/api/get/record/current/week/${recordData.user_id}?week=${weekDetails.currentWeek}&month=${weekDetails.currentMonth}&year=${weekDetails.currentYear}`
+		  );
+		  const data = response.data.record;
+	
+		  if (data && !data.error) {
+			setRecordData((prevData) => ({
+			  ...prevData,
+			  week: weekDetails.currentWeek,
+			  month: weekDetails.currentMonth,
+			  year: weekDetails.currentYear,
+			  description: data.description ? data.description : '',
+			  solutions: data.solutions ? data.solutions : '',
+			}));
+			setRecordExists(true);
+		  }
+	
+		  setIsLoading(false); // set loading to false once we're done fetching data
+		};
+	
 		getConnection();
-	},[]);
+		getCurrentWeekRecord();
+	  }, []);
 
 	const handleSubmitRecord = (event) => {
 		event.preventDefault();
@@ -79,20 +95,51 @@ function Dashboard() {
 		recordData.month = data.currentMonth;
 		recordData.year = data.currentYear;
 
-		axios
-			.post(`${API_URL}/api/set/record/week`, recordData)
-			.then((response) => {
-				toast.success("Form submitted successfully. Reloading...");
-				Cookies.remove("description");
-				Cookies.remove("solutions");
+		if(recordExists){
+			axios
+				.post(`${API_URL}/api/update/record/week/${recordData.user_id}`, recordData)
+				.then((response) => {
+					toast.success("User data updated Successfully. Redirecting...");
+					setTimeout(() => {
+						window.location.reload();
+					}, 2000);
+				})
+				.catch((error) => {
+					toast.error("Error updating user data. Please try again." + error);
+					console.log(error);
+				});
+		} else {
+			axios
+				.post(`${API_URL}/api/set/record/week`, recordData)
+				.then((response) => {
+					toast.success("Form submitted successfully. Reloading...");
+					Cookies.remove("description");
+					Cookies.remove("solutions");
 
-				setTimeout(() => {
-					window.location.reload();
-				}, 2000);
-			})
-			.catch((error) => {
-				toast.error("Error submitting the form. Please try again.");
-			});
+					setTimeout(() => {
+						window.location.reload();
+					}, 2000);
+				})
+				.catch((error) => {
+					toast.error("Error submitting the form. Please try again.");
+				});
+		}
+	};
+
+	const handleChangeDescription = (e) => {
+		const { value } = e.target;
+		setRecordData((prevFormData) => ({
+			...prevFormData,
+			description: value,
+		}));
+	};
+
+	const handleChangeSolution = (e) => {
+		const { value } = e.target;
+		setRecordData((prevFormData) => ({
+			...prevFormData,
+			solutions: value,
+		}));
 	};
 
 	return (
@@ -117,7 +164,7 @@ function Dashboard() {
 									type="text"
 									value={recordData.description}
 									placeholder="Write your description here."
-									onChange={handleChange}
+									onChange={handleChangeDescription}
 									sx={{
 										"& fieldset": { border: "none" },
 									}}
@@ -139,7 +186,7 @@ function Dashboard() {
 									type="text"
 									value={recordData.solutions}
 									placeholder="Write your problems and solutions here."
-									onChange={handleChange}
+									onChange={handleChangeSolution}
 									sx={{
 										"& fieldset": { border: "none" },
 									}}
@@ -148,9 +195,9 @@ function Dashboard() {
 						</Box>
 					</Box>
 					<Box className="trainee_submission_box">
-						<Button variant="contained" className="save_button_trainee" type="submit" disabled={!isChecked}>
-							Submit
-						</Button>
+							<Button variant="contained" className="save_button_trainee" type="submit" disabled={!isChecked}>
+								{recordExists ? 'Update' : 'Submit'}
+							</Button>
 						<Box className="trainee_submission_box_text">
 							<Checkbox
 								sx={{
