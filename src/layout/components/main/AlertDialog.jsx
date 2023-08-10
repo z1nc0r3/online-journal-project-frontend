ce184cd7990af46b55dc0f01a1873edfa1632c05
Lasith Manujitha <Manujitha98@gmail.com>
2023-08-10 13:36:19 +0530

    Add blur effect for prompt

--- src/layout/components/main/AlertDialog.jsx
+++ src/layout/components/main/AlertDialog.jsx
@@ -11,6 +11,15 @@ class AlertDialog extends React.Component {
 	render() {
 		const { open, onClose, onAgree, onDisagree, title, description, sub_description="", agreeText, disagreeText } = this.props;
 
+		// blur out the background root element when the dialog is open
+		console.log("open: ", open);
+		console.log(this.props);
+		if (open) {
+			document.querySelector("#root").classList.add("blur");
+		} else {
+			document.querySelector("#root").classList.remove("blur");
+		}
+
 		return (
 			<Dialog open={open} onClose={onClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
 				<DialogTitle id="alert-dialog-title">{title}</DialogTitle>
