01d65320cf925db5f5100702bb21e7a0a22c8a6f
Lasith Manujitha <Manujitha98@gmail.com>
2023-10-29 18:55:42 +0530

    Trainee List for Evaluator UI updates

--- src/layout/templates/evaluator/Dashboard.jsx
+++ src/layout/templates/evaluator/Dashboard.jsx
@@ -199,7 +199,7 @@ function TraineeList() {
 												</Box>
 											))}
 
-											<Accordion sx={{ width: "100%", backgroundColor: "#69b7ff", boxShadow: "none", borderRadius: 1.5 }}>
+											<Accordion className="supervisor_review_box" sx={{ width: "100%", backgroundColor: "#69b7ff", boxShadow: "none", borderRadius: 1.5 }}>
 												<AccordionSummary aria-controls="panel1bh-content" id="panel1bh-header">
 													<Typography sx={{ width: "100%", flexShrink: 0, fontWeight: "medium", fontSize: "18px" }}>Supervisor Review</Typography>
 												</AccordionSummary>
--- src/layout/templates/evaluator/traineeList.jsx
+++ src/layout/templates/evaluator/traineeList.jsx
@@ -1,32 +1,49 @@
 import * as React from "react";
-import "../../../assets/css/list.css";
 import { useState, useEffect } from "react";
-import { Box, Container, Button, colors } from "@mui/material";
-import { CssBaseline, Typography, Select, MenuItem } from "@mui/material";
+import { Box, Container } from "@mui/material";
+import { ToastContainer } from "react-toastify";
+import Cookies from "js-cookie";
 import axios from "axios";
-import Accordion from "@mui/material/Accordion";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import AccordionSummary from "@mui/material/AccordionSummary";
-//import CssBaseline from "@mui/material/CssBaseline";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import { CssBaseline, Typography } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import TextField from "@mui/material/TextField";
-import Card from "@mui/material/Card";
-import ModeEditIcon from "@mui/icons-material/ModeEdit";
-import { Link } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import "../../../assets/css/list.css";
+import "react-toastify/dist/ReactToastify.css";
+
+const API_URL = process.env.REACT_APP_BACKEND_API_URL;
 
 function TraineeList() {
-  const [expanded, setExpanded] = React.useState(false);
-  const [trainees, setTrainees] = React.useState([]);
+	const [expanded, setExpanded] = useState(false);
+	const [traineeConnection, setConnection] = useState({});
+	const [recordData, setRecordData] = useState({});
+	const [isLoading, setIsLoading] = useState(true);
 
-  const [formData, setFormData] = useState({});
+	const handleChange = (panel) => (event, isExpanded) => {
+		setExpanded(isExpanded ? panel : false);
+	};
 
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
+	// get the records for the current trainees with approved = 0 using evaluator id
+	const getRecords = async (event) => {
+		try {
+			const response = await axios.get(`${API_URL}/api/get/record/all/approved/evaluator/${Cookies.get("user_id")}`);
+			const data = response.data;
 
-  const getTraineeList = (event) => {
-		axios.get(`${process.env.REACT_APP_BACKEND_API_URL}/api/get/trainee/list`).then((response) => {
+			if (data.error) {
+				console.log(data.error);
+			} else {
+				setRecordData(data.records);
+				setIsLoading(false);
+				console.log(data.records);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	// get filtered trainee list for the evaluator
+	const getTraineeList = async (event) => {
+		try {
+			const response = await axios.get(`${API_URL}/api/get/trainee/list/evaluator/${Cookies.get("user_id")}`);
 			const data = response.data;
 
 			if (data.error) {
@@ -34,131 +51,148 @@ function TraineeList() {
 			} else {
 				const traineesData = data.trainees.reduce((acc, trainee) => {
 					acc[trainee.id] = {
+						fName: trainee.fName,
+						department: trainee.department,
 						supervisor_id: trainee.trainee_connection[0] ? trainee.trainee_connection[0].supervisor_id : "",
-						supervisor: trainee.trainee_connection[0] ? trainee.trainee_connection[0].supervisor_name : "",
 						evaluator_id: trainee.trainee_connection[0] ? trainee.trainee_connection[0].evaluator_id : "",
-						evaluator: trainee.trainee_connection[0] ? trainee.trainee_connection[0].evaluator_name : "",
 					};
 					return acc;
 				}, {});
 
-				setTrainees(data.trainees);
-				setFormData(traineesData);
+				setConnection(traineesData);
 			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		getTraineeList();
+		getRecords();
+	}, []);
+
+	const getMonthName = (monthNumber) => {
+		const date = new Date();
+		date.setMonth(monthNumber - 1);
+
+		return date.toLocaleString("en-US", {
+			month: "long",
 		});
 	};
 
-  const handleSubmit = (event, trainee) => {
-		event.preventDefault();
+	// if the data is loading
+	if (isLoading) {
+		return (
+			<Container component="main" className="list_container" maxWidth={false}>
+				<CssBaseline />
 
-		// Assuming trainee is the current trainee object being submitted
-		const updatedTrainee = {
-			...trainee,
-			trainee_connection: {
-				supervisor_id: formData[trainee.id]?.supervisor_id,
-				supervisor_name: formData[trainee.id]?.supervisor,
-				evaluator_id: formData[trainee.id]?.evaluator_id,
-				evaluator_name: formData[trainee.id]?.evaluator,
-			},
-		};
+				<Box className="list_box">
+					<Typography sx={{ width: "100%", flexShrink: 0, fontWeight: "medium", fontSize: "16px" }}>Loading...</Typography>
+				</Box>
+			</Container>
+		);
+	}
 
-		axios
-			.post(`${process.env.REACT_APP_BACKEND_API_URL}/api/update/assign/`, updatedTrainee)
-			.then((response) => {
-				toast.success(response.data.message);
-				setTimeout(() => {
-					window.location.reload();
-				}, 2000);
-			})
-			.catch((error) => {
-				toast.error(error.response.data.message);
-			});
-	};
-  
-  useEffect(() => {
-		getTraineeList();
-	}, []);
+	return (
+		<Container component="main" className="list_container" maxWidth={false}>
+			<CssBaseline />
 
-  return (
-    <Container component="main" className="list_container" maxWidth={false}>
-      <CssBaseline />
+			<ToastContainer />
 
-      <ToastContainer />
-
-		<Box className="list_box">
-				{trainees.map((trainee, i) => (
+			<Box className="list_box">
+				{Object.keys(recordData).map((trainee, i) => (
 					<Accordion
 						expanded={expanded === i}
 						onChange={handleChange(i)}
 						sx={{ width: "100%", backgroundColor: "#dfefff", boxShadow: "none", marginBottom: "10px", borderRadius: "4px" }}
 						key={i}
 						className="accordion_item">
-							
 						<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1bh-content" id="panel1bh-header">
-							
-							<Typography sx={{ width: "5%", flexShrink: 0, fontWeight: "medium", fontSize: "16px" }}>{i + 1}</Typography>
-							<Typography sx={{ width: "66%", flexShrink: 0, fontWeight: "medium", fontSize: "16px" }}>{trainee.fName}</Typography>
-							<Typography sx={{ color: "text.secondary", fontSize: "14px" }}>{trainee.department}</Typography>
+							<Typography sx={{ marginRight: "1rem", fontSize: "18px" }}>{i + 1}</Typography>
+							<Typography sx={{ width: "66%", flexShrink: 0, fontWeight: "medium", fontSize: "16px" }}>{traineeConnection[trainee].fName}</Typography>
+							<Typography sx={{ color: "text.secondary", fontSize: "14px" }}>{traineeConnection[trainee].department}</Typography>
 						</AccordionSummary>
 
 						<AccordionDetails>
-				{Array.from({length: 5}, (_, i) => (
-					<Box  sx={{ width: "100%", backgroundColor: "#9dd0ff", boxShadow: "none", marginBottom: "10px", borderRadius: "4px" }}> 
-					    <Accordion className="month_item">
-							<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="month1-content" id="month1-header">
-								<Typography sx={{ fontWeight: "bold", textAlign: "center", color: "#414141",paddingLeft: 0.5 }}>{`Month ${i+1}`}</Typography>
-							</AccordionSummary>
+							{Object.keys(recordData[trainee]["months"]).map((month, j) => (
+								<Box className="list_container" key={j}>
+									<Accordion className="month_item">
+										<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="month1-content" id="month1-header">
+											<Typography sx={{ fontWeight: "bold", textAlign: "center", color: "#414141", paddingLeft: 0.5 }}>{getMonthName(month)}</Typography>
+										</AccordionSummary>
 
-						    <AccordionDetails>
-								{Array.from({length: 4}, (_, i) => (
-									<Box sx={{ width: "100%", backgroundColor: "#319fd6", boxShadow: "none", marginBottom: "10px", borderRadius: "4px" }}>
-										<Accordion sx={{ width: "100%", backgroundColor: "#b2d9fe", boxShadow: "none", marginBottom: "10px", borderRadius: "4px" }}>
-											<AccordionSummary>
-											<Typography sx={{ width: "100%", flexShrink: 0, fontWeight: "medium", fontSize: "16px" }}>{`Week ${i+1}`}</Typography>
-											</AccordionSummary>
-											<AccordionDetails  >
-												<Box className="weekly_report_container">
-													<Typography className="report_title report_title_des">Description :</Typography>
-													<Box className="weekly_report_des">
-													<Typography sx={{ fontSize: "16px", textAlign: "left" }}></Typography>
-													</Box>
+										<AccordionDetails>
+											{Object.keys(recordData[trainee]["months"][month]["weekly"]).map((week, k) => (
+												<Box className="list_container" key={k}>
+													<Accordion sx={{ width: "100%", backgroundColor: "#9dd0ff", boxShadow: "none", marginBottom: "10px", borderRadius: "4px" }} className="accordion_item">
+														<AccordionSummary>
+															<Typography sx={{ width: "100%", flexShrink: 0, fontWeight: "medium", fontSize: "16px" }}>{`Week ${recordData[trainee]["months"][month]["weekly"][week].week}`}</Typography>
+														</AccordionSummary>
+														<AccordionDetails className="report_detail_container">
+															<Box className="weekly_report_container">
+																<Typography className="report_title report_title_des">Description :</Typography>
+																<Box className="weekly_report_des">
+																	<Typography sx={{ fontSize: "16px", textAlign: "left" }}>{recordData[trainee]["months"][month]["weekly"][week].description}</Typography>
+																</Box>
+															</Box>
+															<Box className="weekly_report_container" sx={{ marginTop: 2 }}>
+																<Typography className="report_title report_title_des">Solutions :</Typography>
+																<Box className="weekly_report_des">
+																	<Typography sx={{ fontSize: "16px", textAlign: "left" }}>{recordData[trainee]["months"][month]["weekly"][week].solutions}</Typography>
+																</Box>
+															</Box>
+														</AccordionDetails>
+													</Accordion>
 												</Box>
-											</AccordionDetails>
-										</Accordion>
-									</Box>
-									
-								))}
+											))}
 
-								<Box>
-								<Accordion sx={{width: "100%",backgroundColor: "#078be8",boxShadow: "none",}}>
-									<AccordionSummary>
-									<Typography>Supervisor Report</Typography>
-									</AccordionSummary>
-								</Accordion>
+											<Accordion className="supervisor_review_box" sx={{ width: "100%", backgroundColor: "#69b7ff", boxShadow: "none", borderRadius: 1.5 }}>
+												<AccordionSummary aria-controls="panel1bh-content" id="panel1bh-header">
+													<Typography sx={{ width: "100%", flexShrink: 0, fontWeight: "medium", fontSize: "18px" }}>Supervisor Review</Typography>
+												</AccordionSummary>
+
+												<Box className="supervisor_report_field" variant="outlined">
+													<div className="assigned_student">
+														<Typography component={"span"} variant="body1">
+															{recordData[trainee]["months"][month]["reports"]}
+														</Typography>
+													</div>
+												</Box>
+
+												<Box className="supervisor_leaves_field" variant="outlined" sx={{ padding: 3 }}>
+													<Typography sx={{ width: "80%", flexShrink: 0, fontWeight: "medium", fontSize: "18px", textAlign: "left" }}>Number of leaves</Typography>
+													<div className="text_align_right">
+														<Typography component={"span"} className="leaves_count">
+															{recordData[trainee]["months"][month]["number_of_leave"]}
+														</Typography>
+													</div>
+												</Box>
+											</Accordion>
+										</AccordionDetails>
+									</Accordion>
+								</Box>
+							))}
+
+							<Accordion className="evaluator_report_field" sx={{ width: "100%", backgroundColor: "#69b7ff", boxShadow: "none", borderRadius: 1.5, marginTop: 0.5 }}>
+								<AccordionSummary aria-controls="panel1bh-content" id="panel1bh-header">
+									<Typography sx={{ width: "100%", flexShrink: 0, fontWeight: "medium", fontSize: "18px" }}>Evaluator Review</Typography>
+								</AccordionSummary>
+
+								<Box className="evaluator_report_field_sub" variant="outlined">
+									<div className="assigned_student">
+										<Typography component={"span"} variant="body1">
+										{recordData[trainee]["evalReport"]}
+										</Typography>
+									</div>
 								</Box>
 
-							</AccordionDetails>
-						</Accordion>
-					</Box>
-					
-                ))}
-              </AccordionDetails>
-			  <Box sx={{ width: "100%", backgroundColor: "#9dd0ff", boxShadow: "none", marginBottom: "10px", borderRadius: "4px" }}>
-					  <Accordion sx={{width: "100%",backgroundColor: "#078be8",boxShadow: "none",}}>
-					     <AccordionSummary>
-					       <Typography>Evaluator Report</Typography>
-					     </AccordionSummary>
-					    <AccordionDetails>
-					      <textarea id="w3review" name="w3review" rows="15" cols="75"></textarea>
-					      <Button variant="contained" type="submit" className="register_button" sx={{ width: "89%", bgcolor: "#379fff", fontSize: "16px" }}>Update</Button>
-					    </AccordionDetails>
-					   </Accordion>
-					</Box>
+							</Accordion>
+						</AccordionDetails>
 					</Accordion>
 				))}
 			</Box>
-    </Container>
-  );
+		</Container>
+	);
 }
 
-export default TraineeList
+export default TraineeList;
